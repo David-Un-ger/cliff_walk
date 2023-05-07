cd031c48e7b20ad60c88089d7df25618f8598544
Nils Uhlemann <nils.uhlemann@web.de>
2023-05-07 08:43:21 +0200

    moved train() and plot_training() implementations from strategy interface to tdl strategy interface

--- src/strategy.py
+++ src/strategy.py
@@ -1,7 +1,6 @@
 from typing import Tuple
 from abc import ABC, abstractmethod
 from matplotlib.pyplot import Axes
-import pandas as pd
 
 from gymnasium import Env
 
@@ -14,27 +13,9 @@ class Strategy(ABC):
     def __init__(self, name: str):
         self.name = name
 
+    @abstractmethod
     def train(self, env: Env, max_nr_steps: int = 10000):
-        observation, _ = env.reset()
-        steps_since_reset = 0
-        self.train_sequences = list()
-        for train_step_idx in range(max_nr_steps):
-            env, observation, cancelled, terminated = self.step(env,
-                                                                observation,
-                                                                train=True)
-            steps_since_reset += 1
-
-            if cancelled or terminated:
-                print(
-                    f"{self.name}: {'Reached the goal' if terminated else 'Fell of the cliff'} in {steps_since_reset} steps after {train_step_idx+1} training steps"
-                )
-                self.train_sequences.append({
-                    "steps_since_reset": steps_since_reset,
-                    "reached_goal": terminated,
-                    "training_step": train_step_idx + 1
-                })
-                steps_since_reset = 0
-                observation, _ = env.reset()
+        pass
 
     def run(self, env: Env, max_nr_steps: int = 1000):
         observation, _ = env.reset()
@@ -49,43 +30,9 @@ class Strategy(ABC):
                 )
                 break
 
+    @abstractmethod
     def plot_training(self, ax: Axes):
-        df_training_result = pd.DataFrame(self.train_sequences)
-        df_training_result.plot(x="training_step",
-                                y="steps_since_reset",
-                                ax=ax,
-                                linestyle="--",
-                                color="grey",
-                                zorder=1,
-                                label=None)
-
-        df_goal = df_training_result[df_training_result.reached_goal == True]
-        df_cliff = df_training_result[df_training_result.reached_goal == False]
-
-        df_goal.plot.scatter(x="training_step",
-                             y="steps_since_reset",
-                             s=30,
-                             ax=ax,
-                             marker="o",
-                             color="green",
-                             zorder=2,
-                             label="Reached the goal")
-
-        df_cliff.plot.scatter(x="training_step",
-                              y="steps_since_reset",
-                              s=30,
-                              ax=ax,
-                              marker="x",
-                              color="red",
-                              zorder=2,
-                              label="Fell off the cliff")
-
-        ax.grid("on")
-        ax.set_axisbelow(True)
-        ax.set_xlabel("Training step")
-        ax.set_ylabel("Steps since last reset")
-        ax.set_title(
-            f"{self.name} (min. steps: {df_goal.steps_since_reset.min()})")
+        pass
 
     @abstractmethod
     def step(self,
--- src/strategy_tdl.py
+++ src/strategy_tdl.py
@@ -1,6 +1,8 @@
 from abc import ABC, abstractmethod
 import numpy as np
 from typing import Tuple
+from matplotlib.pyplot import Axes
+import pandas as pd
 
 from strategy import Strategy
 from gymnasium import Env
@@ -33,6 +35,28 @@ class TDLStrategy(Strategy, ABC):
                        reward: int):
         pass
 
+    def train(self, env: Env, max_nr_steps: int = 10000):
+        observation, _ = env.reset()
+        steps_since_reset = 0
+        self.train_sequences = list()
+        for train_step_idx in range(max_nr_steps):
+            env, observation, cancelled, terminated = self.step(env,
+                                                                observation,
+                                                                train=True)
+            steps_since_reset += 1
+
+            if cancelled or terminated:
+                print(
+                    f"{self.name}: {'Reached the goal' if terminated else 'Fell of the cliff'} in {steps_since_reset} steps after {train_step_idx+1} training steps"
+                )
+                self.train_sequences.append({
+                    "steps_since_reset": steps_since_reset,
+                    "reached_goal": terminated,
+                    "training_step": train_step_idx + 1
+                })
+                steps_since_reset = 0
+                observation, _ = env.reset()
+
     def step(self,
              env: Env,
              curr_obs: int,
@@ -56,6 +80,44 @@ class TDLStrategy(Strategy, ABC):
             self.update_q_table(action, curr_obs, new_observation, reward)
 
         return env, new_observation, cancelled, terminated
+
+    def plot_training(self, ax: Axes):
+        df_training_result = pd.DataFrame(self.train_sequences)
+        df_training_result.plot(x="training_step",
+                                y="steps_since_reset",
+                                ax=ax,
+                                linestyle="--",
+                                color="grey",
+                                zorder=1,
+                                label=None)
+
+        df_goal = df_training_result[df_training_result.reached_goal == True]
+        df_cliff = df_training_result[df_training_result.reached_goal == False]
+
+        df_goal.plot.scatter(x="training_step",
+                             y="steps_since_reset",
+                             s=30,
+                             ax=ax,
+                             marker="o",
+                             color="green",
+                             zorder=2,
+                             label="Reached the goal")
+
+        df_cliff.plot.scatter(x="training_step",
+                              y="steps_since_reset",
+                              s=30,
+                              ax=ax,
+                              marker="x",
+                              color="red",
+                              zorder=2,
+                              label="Fell off the cliff")
+
+        ax.grid("on")
+        ax.set_axisbelow(True)
+        ax.set_xlabel("Training step")
+        ax.set_ylabel("Steps since last reset")
+        ax.set_title(
+            f"{self.name} (min. steps: {df_goal.steps_since_reset.min()})")
 
     def __str__(self):
         return f"{self.q_table}"
